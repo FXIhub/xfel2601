35cbcb5e7cc30fa7b4d27a5bb201a8c7ce007290
Kartik Ayyer <kartik.ayyer@desy.de>
2021-08-10 18:53:26 +0200

    conf_shift1.py: Assembly and masking

--- conf_shift1.py
+++ conf_shift1.py
@@ -21,6 +21,7 @@ from backend import add_record
 import sys
 sys.path.append("/gpfs/exfel/exp/SQS/202131/p900210/usr/Shared/xfel2601")
 import tools.sizelib
+import itertools
 
 # Testing
 # from backend.euxfel import ureg
@@ -58,19 +59,63 @@ state['EuXFEL/FirstCell'] = 0
 # [For simulator, comment if running with full detector, otherwise uncomment]
 # state['EuXFEL/SelModule'] = 0
 
+PREFIX = '/gpfs/exfel/exp/SQS/202102/p002601/'
 detector_distance = 0.55
 wavelength = 1.03e-9
 pixel_size = 204e-6
-n_pulses = 400
-adu_threshold = 10
-hitscore_threshold = 4000
+n_pulses = 2
+adu_threshold = 4
+hitscore_threshold = 1000
 module_numbers = [0,7,8,15]
 
+def _iterating_median(v, tol=3):
+    if len(v) == 0:
+        return 0
+    #vmin, vmax = v.min(), v.max()
+    vmin, vmax = -2*tol, 2*tol
+    vmed = np.median(v[(vmin < v) & (v < vmax)])
+    vmed0 = vmed
+    i = 0
+    while True:
+        vmin, vmax = vmed-tol, vmed+tol
+        vmed = np.median(v[(vmin < v) & (v < vmax)])
+        if vmed == vmed0:
+            break
+        else:
+            vmed0 = vmed
+        i += 1
+        if i > 20:
+            break
+    return vmed
 
-f = h5py.File('/gpfs/exfel/exp/SQS/202131/p900210/usr/Shared/filipe/cal.1627753573.576151.h5','r')
-dark_cal = np.array(f['/DSSC_MiniSDDV1_F2_004/Offset/0/data'])
-#print("dark cal",dark_cal.shape)
+def _common_mode(img):
+    """img should be subtracted by the dark.
+    This version applies on all pixels (no mask)
+    img.shape == (X, Y)"""
+    ig = img.astype('f8').copy()
+    L = 64
+    for i, j in itertools.product(range(ig.shape[0] // L),
+                                  range(ig.shape[1] // L)):
+        img = ig[i*64:(i+1)*64, j*64:(j+1)*64]
+        med = _iterating_median(img.ravel())
+        img -= med
+    return ig
 
+def assem_00(mods, cellind):
+    '''Assembly of inner modules from geom_00 coordinates
+    mods is a list of 4 (512,400,ncells) arrays
+    '''
+    #assem = np.full((289,283), np.nan)
+    assem = np.zeros((289,283))
+    assem[161:161+128,140-128:140] = mods[0][:128,:,cellind].T[:,::-1]
+    assem[ 13: 13+128,128-128:128] = mods[1][:128,:,cellind].T[:,::-1]
+    assem[128-128:128,146:146+128] = mods[2][:128,:,cellind].T[::-1]
+    assem[274-128:274,154:154+128] = mods[3][:128,:,cellind].T[::-1]
+    return assem[::-1,::-1]
+
+with h5py.File(PREFIX+'usr/Shared/aux/badpixel_mask_r0002.h5', 'r') as f:
+    goodpix = f['entry_1/good_pixels'][:][[module_numbers]].transpose(0,2,1)
+    goodpix_stacked = np.concatenate(tuple(goodpix), axis=1)
 
 def onEvent(evt):
     #print(list(evt.keys()))
@@ -80,35 +125,47 @@ def onEvent(evt):
         print("No DSSC data, skipping event")
         return
     proc_rate = analysis.event.printProcessingRate(pulses_per_event=1)
-    #print(evt['photonPixelDetectors']['DSSC00'].data.shape)
-    dets = np.concatenate(tuple([evt['photonPixelDetectors']['DSSC%.2d'%m].data for m in module_numbers]), axis=1)
-    #print(dets.shape)
-    det = evt['photonPixelDetectors']['DSSC00'].data
-    #module = add_record(evt["analysis"], "analysis", "single", det[...,:n_pulses]-dark_cal[:,:,:n_pulses])
-    module = add_record(evt["analysis"], "analysis", "single", dets[...,:n_pulses])
     proc_rate = add_record(evt["analysis"], "analysis", "processing Rate", proc_rate)
 
-    '''
-    analysis.hitfinding.countLitPixels(evt, module, aduThreshold=adu_threshold, hitscoreThreshold=hitscore_threshold, stack=True)
+    #print(evt['photonPixelDetectors']['DSSC00'].data.shape)
+    mods = [evt['photonPixelDetectors']['DSSC%.2d'%m].data for m in module_numbers]
+
+    stacked = np.concatenate(tuple(mods), axis=1)
+    #stacked *= goodpix_stacked[:,:,np.newaxis]
+    #stacked = np.concatenate(tuple([_common_mode(mod) for m in mods]), axis=1)
+    #print(stacked.shape)
+    modules = add_record(evt["analysis"], "analysis", "single", stacked[...,:n_pulses])
+
+    analysis.hitfinding.countLitPixels(evt, modules, 
+                                       aduThreshold=adu_threshold, 
+                                       hitscoreThreshold=hitscore_threshold, 
+                                       mask=goodpix_stacked[:,:,np.newaxis],
+                                       stack=True)
     hitscore = evt["analysis"]["litpixel: hitscore"].data
     hittrain = np.bool8(evt["analysis"]["litpixel: isHit"].data)
     hitscore_pulse = add_record(evt["analysis"], "analysis", "hitscore", hitscore)
-    for i in range(module.data.shape[2]):
+    for i in range(modules.data.shape[-1]):
         hitscore_pulse = add_record(evt["analysis"], "analysis", "hitscore", hitscore[i])
         plotting.line.plotHistory(hitscore_pulse, group="Hitfinding", hline=hitscore_threshold, history=10000)
+    '''
 
-    #print("module shape",module.data.shape)
+    #print("modules shape",modules.data.shape)
     #print("hittrain shape",hittrain.shape)
     for hit_index in np.arange(len(hittrain))[hittrain]:
-        single_hit = add_record(evt["analysis"], "analysis", "Hit", module.data[...,hit_index])
+        single_hit = add_record(evt["analysis"], "analysis", "Hit", modules.data[...,hit_index])
         plotting.image.plotImage(single_hit, history=10)
     '''
 
-    random_image = add_record(evt["analysis"], "analysis", "Random Image", module.data[...,10])
+    random_cell = 0 # Not really random, eh?
+    random_image = add_record(evt["analysis"], "analysis", "Random Image", modules.data[...,random_cell])
     plotting.image.plotImage(random_image, history=10)
 
-    
-    #plotting.image.plotImage(module, send_rate=10)
+    squeezed_image = add_record(evt["analysis"], "analysis", "Squeezed Image", modules.data[...,random_cell])
+    plotting.image.plotImage(squeezed_image, history=10, aspect_ratio=0.866)
+
+    assem_image = add_record(evt["analysis"], "analysis", "Assem Image", assem_00(mods, random_cell))
+    plotting.image.plotImage(assem_image, history=10, aspect_ratio=2/np.sqrt(3))
+
     if(proc_rate is not None):
         plotting.line.plotHistory(proc_rate, group="Hitfinding")
     return
