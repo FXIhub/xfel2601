f3964204883078f48f88b4be9f261e6ea7908977
Filipe Maia <filipe.c.maia@gmail.com>
2021-08-10 16:45:42 +0200

    Add shebang and allow setting sigma limits

--- offline/badpixel_mask.py
+++ offline/badpixel_mask.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+
 import h5py
 import numpy as np
 
@@ -10,11 +13,17 @@ def main():
     parser.add_argument('-o', '--out_folder', 
                         help='Path of output folder (default=%s/scratch/det/)'%PREFIX,
                         default=PREFIX+'scratch/det/')
+    parser.add_argument('-l', '--low', 
+                        help='Minimum sigma for a good pixel',
+                        type=float, default=0.5)
+    parser.add_argument('-H', '--high', 
+                        help='Maximum sigma for a good pixel',
+                        type=float, default=1.5)
     args = parser.parse_args()
 
     with h5py.File(PREFIX + '/scratch/dark/r%.4d_dark.h5'% args.dark_run, 'r') as f:
         sigma = np.array(f['data/sigma'])
-        mask = ~(sigma.mean(1) < 0.5) | (sigma.mean(1) > 1.5)
+        mask = ~(sigma.mean(1) < args.low) | (sigma.mean(1) > args.high)
 
     with h5py.File(args.out_folder+"/"+"badpixel_mask_r%04d.h5" % args.dark_run, 'a') as outf:
         dset_name = 'entry_1/good_pixels'
