80cb3e18e698d50666dc360280b1c7c1f7bb76de
Filipe Maia <filipe.c.maia@gmail.com>
2021-08-11 21:57:34 +0200

    Update dark run

--- offline/slurm/radialavg_array.sh
+++ offline/slurm/radialavg_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --array=35-56
+#SBATCH --array=5-13
 #SBATCH --time=04:00:00
 #SBATCH --partition=upex-beamtime
 #SBATCH --reservation=upex_002601
@@ -16,7 +16,7 @@ PREFIX=/gpfs/exfel/exp/SQS/202102/p002601
 source /etc/profile.d/modules.sh
 source ${PREFIX}/scratch/user/ayyerkar/source_this_at_euxfel
 
-DARK_RUN=34
+DARK_RUN=61
 
 python ../radialavg.py ${SLURM_ARRAY_TASK_ID} ${DARK_RUN}
 
