0c0f2113d3e402609efc646145ec62c0d5c1fb5f
Kartik Ayyer <kartik.ayyer@desy.de>
2021-08-12 15:52:16 +0200

    save_hits_array: Added necessary DARK_RUN option

--- offline/slurm/save_hits_array.sh
+++ offline/slurm/save_hits_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --array=57,58
+#SBATCH --array=67
 #SBATCH --time=24:00:00
 #SBATCH --partition=upex-beamtime
 #SBATCH --reservation=upex_002601
@@ -15,6 +15,7 @@ PREFIX=/gpfs/exfel/exp/SQS/202102/p002601
 
 source /etc/profile.d/modules.sh
 source ${PREFIX}/scratch/user/ayyerkar/source_this_at_euxfel
+DARK_RUN=66
 
-python ../save_hits.py ${SLURM_ARRAY_TASK_ID}
+python ../save_hits.py ${SLURM_ARRAY_TASK_ID} ${DARK_RUN}
 
